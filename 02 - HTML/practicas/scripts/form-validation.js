e34b5f24c104ae955be486c1f2084da7ef005dea
Jose Rdz <joseluisrdzs@live.com>
2022-03-05 01:29:36 -0600

    localStorage & requests

--- 02 - HTML/practicas/scripts/form-validation.js
+++ 02 - HTML/practicas/scripts/form-validation.js
@@ -1,20 +1,37 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
   'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
   var forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+          event.preventDefault();
+          event.stopPropagation();
         }
 
-        form.classList.add('was-validated')
+        form.classList.add('was-validated');
+        if (form.checkValidity()) {
+          const products = JSON.parse(localStorage.getItem("products")) || [];
+          let requests = JSON.parse(localStorage.getItem("requests")) || [];
+          console.log(requests);
+
+          requests.push({
+            firstName: document.getElementById('firstName').value,
+            lastName: document.getElementById('lastName').value,
+            email: document.getElementById('email').value,
+            address: document.getElementById('address').value,
+            tel: document.getElementById('tel').value,
+            cardName: document.getElementById('cc-name').value,
+            cardNumber: document.getElementById('cc-number').value,
+            expiration: document.getElementById('cc-expiration').value,
+            products
+          });
+
+          localStorage.setItem('requests', JSON.stringify(requests));
+          document.location.href = './index.html';
+        }
       }, false)
     })
 })()
